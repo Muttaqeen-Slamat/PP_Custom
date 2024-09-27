bc5776d262437af60702262212fb6b4e409a186d
Muttaqeen Slamat <muttaqeens@gmail.com>
2024-09-27 08:48:09 +0200

    auto no button if no stock

--- pp_shopify_remove_btn_if_stock_out.js
+++ pp_shopify_remove_btn_if_stock_out.js
@@ -1,7 +1,8 @@
-// only to be used on the console. 
 // sees if there is a innerHTML that has sold out and then removes the customize button.
+// for settings custom javascript.
 
-if(document.getElementsByClassName('price price--large price--sold-out price--show-badge')[0].innerHTML || document.getElementsByClassName('price price--large price--sold-out price--on-sale price--show-badge')[0].innerHTML){
+if(document.getElementsByClassName('price price--large price--sold-out price--show-badge')[0]?.innerHTML || document.getElementsByClassName('price price--large price--sold-out price--on-sale price--show-badge')[0]?.innerHTML){
     document.querySelector('#pp_main_btn_sec').classList.add('hidden')
+    document.getElementsByClassName('shopify-payment-button__button shopify-payment-button__button--unbranded')[0].innerHTML = 'Out of Stock'
     console.log('Removed through adding a class of hidden if sold out exists')
 }
